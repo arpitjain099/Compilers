80c593157a7faedfb6364ec8b5b3f9189bce84fb
Arpit <arpitjain099@gmail.com>
2013-11-23 06:18:29 +0530

    compilers first addition

---
+++ errors/error_full.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int hello3()
+{
+	int a;
+	int b;
+}
+int hello(int a)
+{
+	int a;
+	char bd;
+}
+int main() 
+{
+char bd=4;
+int a=4.5;
+float k=4;
+char ch=1.5;
+int a,2;
+int x,y,c,d=1;
+int a,c=bd;
+int a=b++; 
+b=4;			
+a=hello78(5);	// hello78 not defined
+a=hello(bd);  //bd is char
+functoprint();
+a=hello4(5);
+{}
+enum fruit {
+    apple,
+	mango,
+	lichi,
+	apple
+};
+
+struct name{
+char fname;
+char lname;
+char fname;
+};
+
+for(a=0;i<4;i++)
+{}
+if(a==2){int a;}
+else {a=5;b=8;}
+
+while(a<10){}
+}
+int hello4(int b)
+{
+int a;
+}
